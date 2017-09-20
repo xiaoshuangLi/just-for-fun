ec10f59667e37246ac20d0aaaf97de0053413b20
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-09-20 12:10:36 +0800

    add github ribbon

--- frontend/js/containers/App.js
+++ frontend/js/containers/App.js
@@ -82,6 +82,9 @@ class App extends AppExtend {
 		return (
 			<div className="body">
 			  <Style className="svg-hide" width="0" height="0"/>
+{/* 			  <a href="https://github.com/you">
+			  	<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" />
+			  </a> */}
 				<Loading show={!isLoaded}>
 					<Preload present={present}/>
 				</Loading>
